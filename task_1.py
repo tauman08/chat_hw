5f4ed2b12b2255e61f30acbf4d8df08d26f4fba5
a.timofeev <tauman08@gmail.com>
2023-05-19 11:39:19 +0300

    1

--- task_1.py
+++ task_1.py
@@ -1,4 +1,11 @@
-# домашнее задание к первому уроку
-#1
-#2
-#3
+#Задание 1
+
+
+
+#1.
+from typing import List
+lst_str = ['разработка', 'сокет', 'декоратор']
+for el in lst_str:
+    print(f'строка "{el}" тип {type(el)}')
+    str_utf = el.encode("utf-8")
+    print(f'строка "{el}" в Unicode(utf-8): "{str_utf}" тип: "{type(str_utf)}"')
